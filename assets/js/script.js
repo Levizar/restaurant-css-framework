b34010d51e65724dc0177cb4cb35146fc2d87666
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2019-10-10 16:13:49 +0200

    final flash

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,30 @@ function test(){
 }
 
 
+function jour2(){
+    document.getElementById('theme').style.backgroundColor='white';
+    document.getElementById('theme').style.backgroundColor='white';
+    document.getElementById('theme').style.color='black';
+    document.getElementById('formulaire').style.backgroundColor='gray';
+    let x = document.querySelectorAll(".cadre");
+    console.log(x)
+    for (let i = 0; i < x.length; i++) {
+      console.log(x[i])
+      x[i].style.backgroundColor = "#eeeeee";
+    }
+  }
+
+  function nuit2(){
+    document.getElementById('theme').style.backgroundColor='#292b2c';
+    document.getElementById('theme').style.color='white';
+    document.getElementById('formulaire').style.backgroundColor='black';
+    document.querySelector('#js-color').style.color='white'; 
+    let x = document.querySelectorAll(".cadre");
+    console.log(x)
+    for (let i = 0; i < x.length; i++) {
+      console.log(x[i])
+      x[i].style.backgroundColor = "#9e9e9e";
+    }
+  }
+
 
